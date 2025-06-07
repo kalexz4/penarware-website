3085b0e0d7a713730a02c05ef86ed1470c716855
f3d5 <michaelhawk.inyourass@gmail.com>
2025-06-07 07:54:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -55,3 +55,61 @@ document.addEventListener("DOMContentLoaded", () => {
     cursorOutline.style.left = `${clientX}px`;
   });
 });
+
+tsParticles.load("tsparticles", {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: "#ffffff"
+    },
+    shape: {
+      type: "circle"
+    },
+    opacity: {
+      value: 0.5,
+      random: false
+    },
+    size: {
+      value: 3,
+      random: true
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"
+      },
+      onclick: {
+        enable: true,
+        mode: "push"
+      },
+      resize: true
+    }
+  },
+  retina_detect: true
+});
+
